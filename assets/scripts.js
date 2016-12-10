21fcf340bb0bb7fbbc764da5c3cf9c87a17b7db6
panu <panu@oksiala.fi>
2016-12-11 00:21:50 +0200

    Doors can open

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,25 +15,48 @@ var DLR = [W-M, H-M];
 var DUM = [W/2, M];
 var DLM = [W/2, H-M];
 
+var DW = W/2 - M;
+
 var FPS = 30;
 
+var SPEED = 0;
+
+var DSTATUS = 1.00;
+
+var DMOVING = 0;
+var DSPEED = 0.05;
 
 function main() {
     var c=document.getElementById("canv");
     var ctx=c.getContext("2d");
-    loop = setInterval(function() {
+    var loop = setInterval(function() {
         draw(ctx);
         update();
     }, 1000/FPS);
 }
 
 function update() {
-
+    console.log(DMOVING);
+    DSTATUS += DMOVING* DSPEED;
+    if (DSTATUS <= 0.0 ) {
+        DSTATUS = 0.0;
+        DMOVING = 0;
+    }
+    if (DSTATUS >= 1.0) {
+        DSTATUS = 1.0;
+        DMOVING = 0;
+    }
+    console.log(DSTATUS);
 }
 
 function draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(0, 0, W, H);
+    ctx.fillStyle = "red";
+    ctx.fill();
     drawWalls(ctx);
     drawDoors(ctx);
+
 }
 
 function drawWalls(ctx) {
@@ -48,8 +71,14 @@ function drawWalls(ctx) {
 }
 
 function drawDoors(ctx) {
-    var leftDoor = [DUL, DUM, DLM, DLL];
-    var rightDoor = [DUR, DUM, DLM, DLR];
+    var leftDoor = [DUL,
+        [M+ DW * DSTATUS, M],
+        [M+ DW * DSTATUS, H-M],
+        DLL];
+    var rightDoor = [DUR,
+        [W - M - DW * DSTATUS, M],
+        [W - M - DW * DSTATUS, H-M],
+        DLR];
     drawPoly(ctx, "#636062", leftDoor);
     drawPoly(ctx, "#636062", rightDoor);
 }
@@ -70,23 +99,43 @@ function drawPoly(ctx, fillStyle, corners) {
     ctx.fill();
 }
 
+function changeSpeed(amount) {
+    if (Math.abs(SPEED + amount) <= 3) {
+        SPEED += amount
+    }
+}
+
 window.addEventListener('keydown', function(event) {
     switch (event.keyCode) {
 
         case 38: // Up
-            console.log("up");
-        break;
+            changeSpeed(1);
+            break;
 
         case 40: // Down
-            console.log("down");
-        break;
+            changeSpeed(-1);
+            break;
 
-        case 32: // Space
-            console.log("space");
-        break;
-        }
+        case 32:
+            if (doorsOpen()) {
+                DMOVING = 1;
+            } else if (doorsClosed()) {
+                DMOVING = -1;
+            } else {
+                DMOVING = DMOVING * (-1);
+            }
+            break;
+    }
 }, false);
 
+
+function doorsOpen() {
+    return DSTATUS == 0.0;
+}
+function doorsClosed() {
+    return DSTATUS == 1.0;
+
+}
 
 window.onload = main();
 
